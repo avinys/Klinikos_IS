3593ac65bfc7908cd215f06db4af2dfbb29a1a7a
Arvydas Vingis <arvydasvinys@gmail.com>
2024-11-09 23:22:26 +0200

    Refactored Kliniku_IS to Klinikos_IS for clarity

--- Klinikos_IS/src/main/java/com/example/klinikos_is/KlinikosIsApplication.java
+++ Klinikos_IS/src/main/java/com/example/klinikos_is/KlinikosIsApplication.java
@@ -1,14 +1,14 @@
-package com.example.kliniku_is;
+package com.example.klinikos_is;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.example.kliniku_is")
-public class KlinikuIsApplication {
+@ComponentScan("com.example.klinikos_is")
+public class KlinikosIsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KlinikuIsApplication.class, args);
+		SpringApplication.run(KlinikosIsApplication.class, args);
 	}
 }
--- Klinikos_IS/src/main/java/com/example/klinikos_is/controllers/AdminController.java
+++ Klinikos_IS/src/main/java/com/example/klinikos_is/controllers/AdminController.java
@@ -1,4 +1,4 @@
-package com.example.kliniku_is;
+package com.example.klinikos_is;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
